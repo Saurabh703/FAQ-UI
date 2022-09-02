2c650ce6b94e25c0beebb14d40610b145dceaaa7
Saurabh Pilare <78712987+Saurabh703@users.noreply.github.com>
2022-09-02 22:34:08 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import FAQ from './FAQ';
+import Header from './Header';
 
 function App() {
+  const [faqs, setFaqs] = useState([
+    {
+      question: 'How many programmers does it take to screw in a lightbulb?',
+      answer: 'None. We don\'t address hardware issue.',
+      open: true
+    },
+    {
+      question: 'What is IoT in simple words?',
+      answer: 'The Internet of Things (IoT) refers to a system of interrelated, internet-connected objects that are able to collect and transfer data over a wireless network without human intervention.',
+      open: false
+    },
+    {
+      question: 'What are examples of IoT devices?',
+      answer: 'Connectivity. This doesn\'t need too much further explanation. ',
+      open: false
+    }
+  ])
+
+  const toggleFAQ = index => {
+    setFaqs(faqs.map((faq, i) => {
+      if (i === index) {
+        faq.open = !faq.open
+      } else {
+        faq.open = false;
+      }
+      return faq
+    }))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header/>
+     <div className="faqs">
+       {faqs.map((faq, i) => (
+         <FAQ faq={faq} index={i} toggleFAQ={toggleFAQ} />
+  ))}
+     </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+           <h1> FAQ page</h1> 
+        </header>
+    )
+}
+
+export default Header
